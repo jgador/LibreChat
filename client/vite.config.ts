4e68fe2b5ac9293b42c766a4efe25d8aace84ef7
Jesse Gador <jgador@netreo.com>
2025-08-03 09:23:18 +0800

    Update logo and favicon from LibreChat to PisoChatGPT

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -52,8 +52,8 @@ export default defineConfig(({ command }) => ({
       },
       includeAssets: [],
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'PisoChatGPT',
+        short_name: 'PisoChatGPT',
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
